eb112488ca65d022d978b44101d2f07aac084fc1
Gonzalo Martinez <martinez.gabriel.gonzalo@gmail.com>
2023-09-21 16:58:56 -0300

    Inicio de modelo de autenticación

--- spotify_project/spotify_api_connection/models.py
+++ spotify_project/spotify_api_connection/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Authenticate(models.Model):
+    access_token = models.CharField(max_length=100)
+    is_expired = models.BooleanField(default=False)
+
+    def set_access_token(self, access_token: str) -> None:
+        self.access_token = access_token
+
+    def get_access_token(self) -> str:
+        return self.access_token
+    
+    def set_is_expired(self, is_expired: bool) -> None:
+        self.is_expired = is_expired
+
+    def get_is_expired(self) -> bool:
+        return self.is_expired
+    
